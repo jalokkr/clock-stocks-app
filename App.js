2ea0236e0ec6ce711398ca349f592a1f28aa0a0a
alok kumar jha <jalok302@gmail.com>
2024-07-10 17:57:22 +0530

    changes in app.js to render screens as per roles and as per login state

--- App.js
+++ App.js
@@ -1,11 +1,44 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import SignIn from "./Pages/SignIn";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import Home from "./Pages/Home";
+import Users from "./Pages/Users";
+import Orders from "./Pages/Orders";
+import Stock from "./Pages/Stock";
+import { Provider, useSelector } from "react-redux";
+import { store } from "./redux/store";
 
-export default function App() {
+const Stack = createNativeStackNavigator();
+
+function AppNavigator() {
+  const { user, isLoggedIn } = useSelector((state) => state.auth);
+
+  return (
+    <NavigationContainer>
+      <Stack.Navigator
+        initialRouteName={isLoggedIn && user ? "Home" : "SignIn"}
+      >
+        {isLoggedIn && user ? (
+          <>
+            <Stack.Screen name="Home" component={Home} />
+            <Stack.Screen name="Users" component={Users} />
+            <Stack.Screen name="Orders" component={Orders} />
+            <Stack.Screen name="Stock" component={Stock} />
+          </>
+        ) : (
+          <Stack.Screen name="SignIn" component={SignIn} />
+        )}
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
+
+function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <AppNavigator />
     </View>
   );
 }
@@ -13,8 +46,14 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
   },
 });
+
+export default () => {
+  return (
+    <Provider store={store}>
+      <App />
+    </Provider>
+  );
+};
