94b2637c88e9d7714a9f6f04f599949fcc486ae7
alok kumar jha <jalok302@gmail.com>
2024-07-23 18:18:28 +0530

    chnages in auth api to add logout function

--- api/authApi.js
+++ api/authApi.js
@@ -1,9 +1,27 @@
 import axios from "axios";
+import * as SecureStore from "expo-secure-store";
 
 export const signIn = async (email, password) => {
   const url = `${process.env.EXPO_PUBLIC_BACKEND_API_URL}/users/login`;
+  console.log(url);
   try {
     let response = await axios.post(url, { email, password });
+    return response?.data;
+  } catch (error) {
+    console.error(error);
+    return error?.response?.data;
+  }
+};
+
+export const logOut = async () => {
+  const token = await SecureStore.getItemAsync("user");
+  const url = `${process.env.EXPO_PUBLIC_BACKEND_API_URL}/users/logout`;
+  try {
+    let response = await axios.post(url, {
+      headers: {
+        authorization: `${token}`,
+      },
+    });
     return response?.data;
   } catch (error) {
     console.error(error);
