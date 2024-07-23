570bfb398d09fa59fba4c74532daa9d9c1a425ee
alok kumar jha <jalok302@gmail.com>
2024-07-23 18:19:01 +0530

    changes in order api to add functions of approve order and reject order apis

--- api/orderApi.js
+++ api/orderApi.js
@@ -17,3 +17,38 @@ export const getAllOrders = async () => {
     return error?.response?.data;
   }
 };
+
+export const approveOneOrder = async (id) => {
+  const token = await SecureStore.getItemAsync("user");
+  const url = `${process.env.EXPO_PUBLIC_BACKEND_API_URL}/orders/${id}/approve`;
+  console.log(url)
+  console.log(token)
+  try {
+    let response = await axios.post(url, {}, {
+      headers: {
+        authorization: `${token}`,
+      },
+    });
+    return response?.data;
+  } catch (error) {
+    console.error(error);
+    return error?.response?.data;
+  }
+};
+
+export const rejectOneOrder = async (id) => {
+  const token = await SecureStore.getItemAsync("user");
+
+  const url = `${process.env.EXPO_PUBLIC_BACKEND_API_URL}/orders/${id}/reject`;
+  try {
+    let response = await axios.post(url, {}, {
+      headers: {
+        authorization: `${token}`,
+      },
+    });
+    return response?.data;
+  } catch (error) {
+    console.error(error);
+    return error?.response?.data;
+  }
+};
